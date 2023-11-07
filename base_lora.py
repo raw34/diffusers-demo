7f161adf3eecc3f4918f6f45ad25d9fe73487508
raw34 <raw0034@gmail.com>
2023-11-07 11:26:13 +0800

    feat: update

--- base_lora.py
+++ base_lora.py
@@ -1,11 +1,14 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
-model_path = "stabilityai/stable-diffusion-xl-base-1.0"
-lora_model_path = "ostris/ikea-instructions-lora-sdxl"
+# model_path = "stabilityai/stable-diffusion-xl-base-1.0"
+# lora_model_path = "ostris/ikea-instructions-lora-sdxl"
+model_path = "models/stable-diffusion-xl-base-1.0"
+lora_model_path = "models/pixel-art-xl"
 pipe = StableDiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16)
 pipe.unet.load_attn_procs(lora_model_path)
-pipe.to("cuda")
+
+pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
 prompt = "A pokemon with blue eyes."
 image = pipe(prompt, num_inference_steps=30, guidance_scale=7.5, cross_attention_kwargs={"scale": 0.5}).images[0]
