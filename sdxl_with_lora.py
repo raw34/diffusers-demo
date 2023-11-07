16f29daf5bc89879aaf3a9d05fffd31cb998dc0a
raw34 <raw0034@gmail.com>
2023-11-07 15:40:55 +0800

    feat: update

--- sdxl_with_lora.py
+++ sdxl_with_lora.py
@@ -3,21 +3,21 @@ import torch
 from diffusers import StableDiffusionXLPipeline
 
 # 模型路径
-model_path = "models/sd_xl_base_1.0.safetensors"
-lora_model_path = "models/pixel-art-xl.safetensors"
+model_path = "models/sd/sd_xl_base_1.0.safetensors"
+# lora_model_path = "models/lora/pixel-art-xl.safetensors"
 
 # 初始化Pipeline
 pipe = StableDiffusionXLPipeline.from_single_file(model_path)
-pipe.load_lora_weights(lora_model_path)
+# pipe.load_lora_weights(lora_model_path)
 
 # LoRA one.
-lora_model_path_1 = "models/cyborg_style_xl-off.safetensors"
-pipe.load_lora_weights(lora_model_path_1)
+lora_model_path_1 = "models/lora"
+pipe.load_lora_weights(lora_model_path_1, weight_name="cyborg_style_xl-off.safetensors")
 pipe.fuse_lora(lora_scale=0.7)
 
 # LoRA two.
-lora_model_path_2 = "models/pikachu.safetensors"
-pipe.load_lora_weights(lora_model_path_2)
+lora_model_path_2 = "models/lora"
+pipe.load_lora_weights(lora_model_path_2, weight_name="pikachu.safetensors")
 pipe.fuse_lora(lora_scale=0.7)
 
 # 确定设备
