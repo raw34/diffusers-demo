fb42c4472347a819ede44b2f3762afcde952b54c
raw34 <raw0034@gmail.com>
2023-11-07 10:21:39 +0800

    feat: update

--- base.py
+++ base.py
@@ -2,12 +2,12 @@ from diffusers import DiffusionPipeline
 from diffusers import StableDiffusionPipeline
 import torch
 
-# model_path = "stabilityai/stable-diffusion-xl-base-1.0"
-# pipe = DiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+model_path = "stabilityai/stable-diffusion-xl-base-1.0"
+pipe = DiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
 
 # 加载模型
-model_path = "./models/sd_xl_base_1.0.safetensors"
-pipe = StableDiffusionPipeline.from_single_file(model_path, use_safetensors=True)
+# model_path = "./models/sd_xl_base_1.0.safetensors"
+# pipe = StableDiffusionPipeline.from_single_file(model_path, use_safetensors=True)
 # pipe.save_pretrained("/Users/randy/Downloads/models/sd")
 
 # 将模型转移到合适的设备
@@ -20,4 +20,4 @@ prompt = "An astronaut riding a green horse"
 
 images = pipe(prompt=prompt).images[0]
 
-images.show()
+images.save("output/base.png")
