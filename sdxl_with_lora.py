a6d3fd9e28597211be04fe14ab6ed9b1ea6e87d9
raw34 <raw0034@gmail.com>
2023-11-07 14:52:30 +0800

    feat: update

--- sdxl_with_lora.py
+++ sdxl_with_lora.py
@@ -2,34 +2,44 @@ import datetime
 import torch
 from diffusers import StableDiffusionXLPipeline
 
+# 模型路径
 model_path = "models/stable-diffusion-xl-base-1.0/sd_xl_base_1.0.safetensors"
 lora_model_path = "models/pixel-art-xl/pixel-art-xl.safetensors"
 
-# 创建输出文件夹的代码可以放在这里，如果文件夹不存在的话
-# os.makedirs("output", exist_ok=True)
-
+# 初始化Pipeline
 pipe = StableDiffusionXLPipeline.from_single_file(model_path)
 pipe.load_lora_weights(lora_model_path)
 
+# 确定设备
 device = "cuda" if torch.cuda.is_available() else "cpu"
 pipe = pipe.to(device)
 
-# 使用torch.Generator来设置种子
+# 设置种子
 seed = 12345
 generator = torch.Generator(device=device).manual_seed(seed)
 
+# 设置参数
 prompt = "a photo of an astronaut riding a horse on mars"
 negative_prompt = ""
-
 num_inference_steps = 30
+guidance_scale = 7.5  # 控制prompt的影响，较大的值将生成与prompt更紧密相关的图像
+width = 1024           # 生成图像的宽度
+height = 1024          # 生成图像的高度
+num_images = 1        # 生成图像的数量
 
-image = pipe(
+# 生成图像
+images = pipe(
     generator=generator,
     prompt=prompt,
     negative_prompt=negative_prompt,
     num_inference_steps=num_inference_steps,
-).images[0]
+    guidance_scale=guidance_scale,
+    width=width,
+    height=height,
+    num_images=num_images,
+).images
 
-# 生成文件名，包含当前时间
-filename = f"output/sdxl_with_lora_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.png"
-image.save(filename)
+# 保存图像
+for i, image in enumerate(images):
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    image.save(f"output/sdxl_{timestamp}_{i}.png")
